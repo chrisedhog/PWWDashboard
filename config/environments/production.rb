ae58c31ef1074f0eb2238fe4f35a9e3447b43573
Chris Hogben <chris.edhogben@gmail.com>
2016-05-14 19:23:10 +1000

    app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => 'heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'herokuapp.com' }
   #Action mailer config:
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -83,7 +83,7 @@ Rails.application.configure do
   :authentication => :plain,
   :user_name      => 'postmaster@sandbox3ed8de9510c845c68bf0d867a8f25394.mailgun.org',
   :password       => ENV['MAILGUN_PASSWORD'],
-  :domain         => 'heroku.com',
+  :domain         => 'herokuapp.com',
   :enable_starttls_auto => true
   }
 
