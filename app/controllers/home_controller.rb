c49b1dfb957ed389421f735415d5cc9fc7246d92
Chris Hogben <chris.edhogben@gmail.com>
2016-05-22 11:01:19 +1000

    forced redirect in home controller if user hasn't set up a profile.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
 # skip_before_action :authenticate_user!
   def index
+  # Unless a user has already set up a profile, they will be required to create one.
+    unless current_user.profile
+      redirect_to new_profile_path
+    end
   end
 end
