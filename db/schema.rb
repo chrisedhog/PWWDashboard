4477916ce387663aeefe6345f7411e9bbb5304e4
Chris Hogben <chris.edhogben@gmail.com>
2016-06-05 13:40:31 +1000

    set up scaffold, about to set up CSV uploader

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529042623) do
+ActiveRecord::Schema.define(version: 20160605020118) do
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "client"
+    t.string   "project_status"
+    t.string   "project_name"
+    t.date     "completion_date"
+    t.date     "completion_month"
+    t.float    "budget_revenue"
+    t.decimal  "budget_margin"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "helps", force: :cascade do |t|
     t.string   "title"
