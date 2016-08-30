8b2daa670b55a3120c3d0332ca217d90d7c4792f
Chris Hogben <chris.edhogben@gmail.com>
2016-08-30 20:37:39 +1000

    attempted to fix email issues with devise_invitable however this didn't work, it says the columns were already added to the database

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   rolify
   # Include default devise modules. Others available are:
   # , :timeoutable and :omniauthable, , :confirmable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :lockable, :invitable
          
  has_one :profile, dependent: :destroy
  
