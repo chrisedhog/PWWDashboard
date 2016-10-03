9d83b2506fe55a19f88e06e657bcc8376b8b343b
Chris Hogben <chris.edhogben@gmail.com>
2016-10-03 23:18:41 +1100

    trying to query my own hash, not sure how to do 'is bettween this date and that date

--- app/models/search.rb
+++ app/models/search.rb
@@ -28,6 +28,15 @@ validates_date :from_date, :to_date, :on_or_before => :today
         return current_data
     end
 
+    def budget_margin_by_month
+        my_projects = Project.where(id: projects.pluck(:id))
+        my_projects = my_projects.where("project_status = ?", "Contracted") 
+        my_projects = my_projects.where("created_at >= ? AND created_at <= ?", from_date, to_date) 
+        my_projects_sorted = my_projects.group_by_month(:created_at).sum(:budget_margin) 
+        
+        return my_projects_sorted
+    end
+
 
     def self.past_sample_data
         { "2015-01-01" => 3256870.00.to_d,
@@ -42,6 +51,33 @@ validates_date :from_date, :to_date, :on_or_before => :today
           "2015-10-01" => 9200000.00.to_d,
           "2015-11-01" => 9500000.00.to_d,
           "2015-12-01" => 9600328.45.to_d }
+    end
+
+# TRYING TO TURN THE BELOW INTO A HASH, I can then group the data bay the searched dates so that it's serving up the correct data to the correct month
+    def self.past_sample_data2
+        date1 = {:record_date => "2015-01-01", :budget_margin => 3256870.00.to_d}
+        date2 = {:record_date => "2015-02-01", :budget_margin => 3825580.00.to_d}
+        date3 = {:record_date => "2015-03-01", :budget_margin => 4394290.00.to_d}
+        date4 = {:record_date => "2015-04-01", :budget_margin => 5550000.00.to_d}
+        date5 = {:record_date => "2015-05-01", :budget_margin => 6044000.00.to_d}
+        date6 = {:record_date => "2015-06-01", :budget_margin => 7100000.00.to_d}
+        date7 = {:record_date => "2015-07-01", :budget_margin => 7700000.00.to_d}
+        date8 = {:record_date => "2015-08-01", :budget_margin => 8154000.00.to_d}
+        date9 = {:record_date => "2015-09-01", :budget_margin => 8860000.00.to_d}
+        date10 = {:record_date => "2015-10-01", :budget_margin => 9200000.00.to_d}
+        date11 = {:record_date => "2015-11-01", :budget_margin => 9500000.00.to_d}
+        date12 = {:record_date => "2015-12-01", :budget_margin => 9600328.45.to_d}
+        data_last_year = [date1, date2, date3, date4, date5, date6, date7, date8, date9, date10, date11, date12]
+
+        return data_last_year
+    end
+
+    def self.get_budget_margin_histdata_by_search_date(dataset, date_from, date_to)
+        dataset.find_all do |dat|
+            if (dat[:record_date] >= ? AND dat[:record_date] <= ?, date_from, date_to)
+                 dat[:record_date]
+            end
+        end
     end
 
     def self.forecast_sample_data
